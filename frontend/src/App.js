494fb936416cf16ae3b3d64c1077a5422cbeea42
Patrick McCabe <pmccabe.dev@gmail.com>
2023-02-22 10:19:11 +0000

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./img/logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={Logo} alt="logo" className="app-logo" />
+    </div>
+  );
+}
+
+export default App;
